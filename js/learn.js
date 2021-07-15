0bc8ae20803a98b5962c8aaab5d66cc6a1e9660a
Md-shefat-masum <mshefat924@gmail.com>
2021-07-15 16:35:34 +0600

    loop

--- js/learn.js
+++ js/learn.js
@@ -59,44 +59,42 @@ function bazar_track(tk, element, price) {
 
 function calculate_grade(number) {
 
-    if(number < 0 || number > 100){
+    if (number < 0 || number > 100) {
 
         document.getElementById("grade_result").innerHTML = 'give a number between 0 to 100.';
-    
-    }
-    else if(number >= 33 && number < 40){
-    
+
+    } else if (number >= 33 && number < 40) {
+
         document.getElementById("grade_result").innerHTML = 'd'.toUpperCase();
-    
-    }
-    else if(number >= 40 && number < 50){
-    
+
+    } else if (number >= 40 && number < 50) {
+
         document.getElementById("grade_result").innerHTML = 'c'.toUpperCase();
-    
-    }else if(number >= 50 && number < 60){
-    
+
+    } else if (number >= 50 && number < 60) {
+
         document.getElementById("grade_result").innerHTML = 'b'.toUpperCase();
-    
-    }else if(number >= 60 && number < 70){
-    
+
+    } else if (number >= 60 && number < 70) {
+
         console.log('A-'.toUpperCase());
         document.getElementById("grade_result").innerHTML = 'a-'.toUpperCase();
-    
-    }else if(number >= 70 && number < 80){
-    
+
+    } else if (number >= 70 && number < 80) {
+
         console.log('A');
         document.getElementById("grade_result").innerHTML = 'a'.toUpperCase();
-    
-    }else if(number >= 80 && number <= 100){
-    
+
+    } else if (number >= 80 && number <= 100) {
+
         console.log('A+');
         document.getElementById("grade_result").innerHTML = 'a+'.toUpperCase();
-        
-    }else{
-    
+
+    } else {
+
         console.log('you are fail.');
         document.getElementById("grade_result").innerHTML = 'you are fail.';
-    
+
     }
 }
 
@@ -145,3 +143,93 @@ const return_string = (params) => {
 }
 
 
+// while , do while , for loop, recursive function
+let i = 10;
+
+// while(i >= 1){
+//     let mul = i + ' x 2 = ' + ( i * 2 ) ;
+//     console.log( mul );
+
+//     // increment or decrement
+//     i--;
+// }
+
+// i = 1;
+// do {
+//     let mul = i + ' x 2 = ' + (i * 2);
+//     console.log(mul);
+
+//     // increment or decrement
+//     i++;
+// } while (i <= 10)
+
+// let time = 3.50;
+
+// for ( i = 1 ; i <= 10 ; i++) {
+
+//     // if(i == 7){
+//     //     break;
+//     // }
+
+//     let mul = i + ' x 2 = ' + (i * 2);
+//     console.log(mul);
+// }
+
+// console.log(++i); // i = i + 1  ;
+// console.log(++i); //
+// console.log(i); // i++ ++i
+
+// console.log(--i);
+// console.log(i--);
+// console.log(i);
+
+// console.log( i += 3 ); // i = i+3;
+// console.log( i += 3 );
+// console.log( i = i+3 );
+// console.log( i = i+3 );
+
+// i = 10;
+// console.log( i ); // i = i-3
+// console.log( i -= 3 ); 
+// console.log( i -= 3 ); 
+
+let html = '';
+
+for (let index = 1; index <= 12; index++) {
+    html  += `
+        <div class="img">
+            <img src="images/players/player-${index}.png" alt="">
+        </div>
+    `;
+}
+
+document.getElementById('gallery').innerHTML = html;
+
+// let namess = 'amit';
+// namess += " asjdflkajsflk";
+// namess = namess + " asjdflk   ajsflk";
+// console.log(namess);
+
+
+const multiply_table = (multiplicant, multiplier, range) => {
+    let mul = multiplier + ' x '+ multiplicant +' = ' + (multiplier * multiplicant);
+    console.log(mul);
+
+    var node = document.createElement("LI"); // Create a <li> node
+    var textnode = document.createTextNode(mul); // Create a text node
+    node.appendChild(textnode); // Append the text to <li>
+    document.getElementById("mul_result").appendChild(node);
+
+    if (multiplier < range) {
+        return multiply_table(multiplicant, ++multiplier, range);
+    }
+}
+
+// multiply_table(2, 1, 5);
+// console.log('');
+
+// multiply_table(3, 5 , 10);
+// console.log('');
+
+// multiply_table(4, 11, 20);
+// console.log('');
